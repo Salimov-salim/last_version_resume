fbede7786eaef92fd1046be363a0fcbea7dabfb3
Salim Salimov <salim.salimov9897@gmail.com>
2022-01-14 10:14:45 +0400

    first

---
+++ Spring/ResumeDBappJpaSpring/src/main/java/com/company/ResumeDBappJpaSpring/ResumeDBappJpaSpringApplication.java
@@ -0,0 +1,13 @@
+package com.company.ResumeDBappJpaSpring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ResumeDBappJpaSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ResumeDBappJpaSpringApplication.class, args);
+	}
+
+}
---
+++ Spring/ResumeDBappJpaSpring/src/test/java/com/company/ResumeDBappJpaSpring/ResumeDBappJpaSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.company.ResumeDBappJpaSpring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ResumeDBappJpaSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
